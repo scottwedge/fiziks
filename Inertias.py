5ca6d0c1676245781154c5f239b9f7611980b630
Cornwall85 <r804cornwall@gmail.com>
2015-03-28 17:11:46 -0400

    Add Interias.py

---
+++ Inertias.py
@@ -0,0 +1,17 @@
+# intertias
+
+from sympy import *
+
+# Id = Disk 
+# Irodc = Rod center
+# Irode = Rod end
+# Is = Sphere
+# Ip = point
+# Ir = Ring 
+
+Id = .5 * I * (r**2)
+Irodc = (1/12) * m * (l**2)
+Irode = (1/3) * m * (l**2)
+Is = (2/5) * m * (r**2)
+Ip = m * (r**2)
+Ir = m * (r**2)
