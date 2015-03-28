03b5beff4e0d53cb18e725f4da7b268560af1f2e
Cornwall85 <r804cornwall@gmail.com>
2015-03-28 13:48:09 -0400

    Add motion.py
    motion equations

---
+++ motion.py
@@ -0,0 +1,24 @@
+#Motion Equations
+from  sympy import *
+
+
+delta(x) = v * t
+delta(x) = v1 * t + .5 * a * (t**2)
+v2 = v1 + a * t
+v = diff(x,t)
+a = diff(v,t)
+v = integrate(a,t)
+x = integrate(v,t)
+
+ 
+  #projectile motion
+  
+   r = ((v**2) * sin(2*theta))/(2*g)
+   h = ((v**2) * sin(2*theta))/(2*g)
+   
+   v2y = v1y + a * t
+   delta(y) = v1 * t + .5 * a * (t**)
+   v2x = v1x
+   delta(x) = v1 * t
+   
+   triangle = ((x**2) + (y**2))**.5
