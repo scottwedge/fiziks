b97a997d2adff963abfb5c4af6d4968241fd85c7
ash-nazg-durbatuluk <samrcluff@gmail.com>
2015-03-28 13:57:03 -0400

    Add energyEquations.py

---
+++ energyEquations.py
@@ -0,0 +1,11 @@
+#Energy equations
+#m is mass
+#v is velocity
+#h is height
+#g is gravity
+#k is k value of spring
+#x is spring compression 
+
+ke = (0.5) * m * (v**2)
+peGravitational = m * g * h 
+peSpring =  (0.5) * k * (x**2)
