d8321318f36f087282f92a30cd9f27f8b386e43c
Cornwall85 <r804cornwall@gmail.com>
2015-03-28 14:02:19 -0400

    Add momentum equations

---
+++ monentum.py
@@ -0,0 +1,12 @@
+#momentum equations
+
+from sympy import *
+
+ p = m * v
+ I = delta(p)
+ I = f * delta(t)
+ I = m * delta(v)
+ p1 = p2
+ m1 * v1 + m2 * v2 = m1* v3 + m2 * v4
+ m1 * v1 + m2* v2 = (m1 + m2) * v3
+ 
