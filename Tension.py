b539824143d190139ce7bd1d5964cf20ed30ac4b
Cornwall85 <r804cornwall@gmail.com>
2015-03-28 15:00:19 -0400

    Add Tension.py

---
+++ Tension.py
@@ -0,0 +1,16 @@
+# Tension
+
+# T = tension
+# m = mass
+# g = gravity
+# f = force
+# v = velocity
+# r = radius
+# ff = force of friction
+from sympy immport *
+
+T = m * g 
+T = f
+Tnet = m * a
+T = m * a + ff 
+T = (m * v**2)/r
