ac1f619cb3cd8b4873fad7c7e92887ebcd9c1065
Cornwall85 <r804cornwall@gmail.com>
2015-03-28 17:18:35 -0400

    Add Planetary.py

---
+++ Planetary.py
@@ -0,0 +1,14 @@
+ #planetary
+ # f = force
+ # G = universal gravitational constant
+ # M = mass 1
+ # m = mass 2
+ # r = radius
+ # kc = keplers constant
+ # T = period
+ from sympy import *
+ 
+ f = (G * M * m) / (r**2)
+ g = G * m /(r**2)
+ 
+ (K**3)/(T**2)= kc
