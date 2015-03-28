2fdbda5daf137941b79db524791afe32ac2947fe
ash-nazg-durbatuluk <samrcluff@gmail.com>
2015-03-28 14:04:07 -0400

    Add force.py
    Explained variables with comments

--- force.py
+++ force.py
@@ -1,12 +1,16 @@
 #Force Equations
+#m is mass
+#a is acceleration
+#g is gravity
+#mu s the coeficient of kinetic friction
+#theta is the angle
+#r is the radius between the planetary systems
+
 from sympy import *
-
-
-
-
 
 forceNet = m * a
 forceNormal = m * g * sin(theta)
 forceFriction = mu * forceNormal
 forceApplied = m * a
 forcePlanetary = G * massSmall * massBig / r**2
+
