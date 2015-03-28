e942f8162cc869bce78a7f75d6f4468d5d03f4c8
Cornwall85 <r804cornwall@gmail.com>
2015-03-28 19:35:21 -0400

    Add CircularOthers.py

---
+++ CircularOthers.py
@@ -0,0 +1,10 @@
+# Circular others
+
+ from sympy import *
+ 
+ v = (a * r) **.5 ## velocity using acceleration and radius
+ r = (v**2) / a # radius using velocity and acceleration
+ r = (v**2) / g # radius for vertical circles
+ r = (m * v**2)/ f # radius using mass and force
+ mu = (v**2) / r / g # mu
+ 
