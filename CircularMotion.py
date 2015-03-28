847cfb8d52c6f0c79554b466b58f1350b98edc02
Cornwall85 <r804cornwall@gmail.com>
2015-03-28 14:21:06 -0400

    Add CircularMotion.py

---
+++ CircularMotion.py
@@ -0,0 +1,14 @@
+ # circular motion
+ # a = aceleration
+ # v =  velocity
+ # r = radius
+ # vv = vertical circle velocity
+ # vh = horizontal circle velocity
+ # f = force
+ 
+ from sympy import *
+ 
+ a = (v**2)/r
+ vv = (g * r)**.5
+ vh = (r * mu * g)**.5
+ f = (m * (v**2))/r
